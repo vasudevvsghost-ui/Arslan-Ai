030380af894e1daec6a4667f06814fe934cbde92
vasudevvsghost-ui <vasudevvsghost@gmail.com>
2025-11-12 17:37:37 +0530

    Add files via upload

---
+++ session/config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:6783705cdb28ff8f6ce30b3d84996858",
+    BOT_INFO: process.env.BOT_INFO || "GHOST_MD;ARAKKALGHOST;https://files.catbox.moe/jc7r5r.jpg",
+    DATABASE_URL: "postgresql://jshsjsjss_user:kqa1eXoJFeiCSRx6fB7UktHgsfxYR7vY@dpg-d49k222li9vc739t5an0-a.oregon-postgres.render.com/jshsjsjss",
+    PREFIX: process.env.PREFIX || ".",
+    MODE: "private",
+    SUDO: "917012984396",
+}
